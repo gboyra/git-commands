03afc71e41432d7951a5f0ea693874794de56ebe
AZTI_BI\gboyra <gboyra@azti.es>
2022-03-05 17:28:24 +0100

    Add 'Useful shortcuts.R to the repo

---
+++ Useful shortcuts.R
@@ -0,0 +1,35 @@
+# Useful shortcuts
+
+# "Control" + "Shift" + "M" == "%>%" 
+a %>% filter(x < 5)
+
+# "Alt" + "-" == "<-"
+a <- 5
+
+# "Control" + "Shift" + "/" corta una linea larga en varias mas cortas:
+# En un lugar de la mancha de cuyo nombre no quiero acordarme vivia un hidalgo
+# caballero de los de lanza en...
+
+# "Control" + "Z" == Undo
+# "Control" + "Shift" + "Z" == Redo
+# "Alt" + "Shift" + "K" == Open shortcuts list
+
+# "Control" + "Alt" + "X" == Extract a function base on a highlighted chunk of code
+
+# Multiple cursors:
+# "Control" + "Alt" and then:
+# (1) up or down arrows to duplicate lines
+# (2) clic on as many lines as you want to write the same sentence in them
+
+# Compress all folds (expandible sections) in the document
+# "Alt" + "O"             == close
+# "Alt" + "Shift" + "O"   == open
+
+# "Ctrl" + "P" to jump between start and end of a parenthesis
+
+# "Shift" + "Alt" + "G" == Go to line...
+
+# "Ctrl" + "Shift" + "F10" == Restart R session
+
+ 
+  
