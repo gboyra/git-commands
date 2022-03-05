f3dd40a3cc6b256b41efaf21f874bad9558c1a19
AZTI_BI\gboyra <gboyra@azti.es>
2022-03-05 17:04:00 +0100

    Add DataCamp git course.R to the repository git status

---
+++ DataCamp git course.R
@@ -0,0 +1,142 @@
+#++++++++++++++++++++++
+# DataCamp git course #
+#++++++++++++++++++++++
+
+
+# 1. Generalities --------------
+#+++++++++++++++++++++++++++++++
+git init
+
+git add "length.R"
+git commit -m "bla bla bla"
+
+# to amend (correct) a commit you can use --amend
+git commit --amend -m "new message"
+
+git status
+git add "length.R"
+
+# 2. See changes ----------
+#++++++++++++++++++++++++++
+
+# to know what changes are made in current files of the repository:
+git diff
+# changes in a particular file:
+git diff "length.R"
+# changes in a particular file in other directory:
+git diff "data/length.R"
+
+# to compare the state the repository files with those in the staging area:
+git diff -r HEAD
+# -r refers to 
+# HEAD refers to the most recent commit
+
+# to compare the staate of a file with that of the staging area:
+git diff -r HEAD "length.R"
+
+git log -2
+git log --oneline
+# to see a specific file's history:
+git log "length.R"
+
+# to see the details (changes) of a specific commit:
+git show 0da2f7 # hash code
+git show HEAD  # the last commit
+git show HEAD~1  # the previous one
+git show HEAD~2  # the one before, etc...
+
+# to know who made the changes:
+git annotate "length.R"
+
+# to see the changes between two commits:
+git diff hash1 hash2
+git diff HEAD~1 HEAD~3
+
+
+# 3. Remove untracked files -----------
+#++++++++++++++++++++++++++++++++++++++
+
+# to show a list of unwanted (untracked) files:
+git clean -n 
+# to remove unwanted files:
+git clean -f
+# git clean only works with untracked files, it you remove them it is for ever
+
+
+
+# 5. Configuration ------------
+#++++++++++++++++++++++++++++++
+# see how git is configured:
+git config --list
+git config --system  # for every user in this computer
+git config --local
+git config --global 
+git config --global user.email gboyra@azti.es
+
+
+
+# 6. UNDO changes ------------
+#++++++++++++++++++++++++++++++
+
+# to remove an addition to stage, for example:
+git add "analysis.R"  # you reset:
+git reset HEAD
+
+#
+# undo changes that have not yet been staged:
+git checkout -- "length.R"  
+# careful: when you discard changes this way, they are gone forever
+
+# You can combine reset and checkout to do both things
+# First reset the staged changes
+git reset HEAD "length.R"  # or:
+git reset "length.R"
+# And then unstage the file
+git checkout -- "length.R"
+
+# to restore an old version of a file:
+git checkout hash length.R
+# and don't forget to commit the change:
+git commit -m "restore and old version"
+
+# to undo changes to many files at once:
+git reset
+git reset HEAD  # the last or default commit or stage
+git reset HEAD "data"  # a full directory
+
+# to unstage many files at once:
+git checkout data  # all the changed files in the directory
+git checkout -- .  # all the changed files in the current directory
+ 
+
+# 7. Branches ------------
+#+++++++++++++++++++++++++
+
+# One of the reasons of Git's popularity is its support for branches
+# Branches are different versions of your work
+# They are like parallel universes, changes you make in one branch
+# doesn't affect the other branches (until you merge them back together)
+# Normally, a commit has (is the child of) one single father
+# But in merges, the commit has two fathers
+
+# By default, every git repository has a branch called master (or main)
+
+# to list all the branches in a repository, use branch
+git branch
+# the branch you are currently in will be shown with a * beside its name
+
+# to list the differences between branches:
+git diff branch1 branch2
+
+
+# use "checkout" to switch from one branch to another:
+git checkout branch.to.go.to  # you move to your secondary branch
+
+git rm "length.R" # remove file and stages the change in one step
+git commit -m "Removing file length.R"  # commit the removal
+git checkout master   # go back to your primary branch
+ls  # checkout that the file is not there
+
+
+
+
